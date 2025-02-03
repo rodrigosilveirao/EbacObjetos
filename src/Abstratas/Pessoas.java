7ca40677bda4b27c2c9927f229a0451f3f88cf16
Rodrigo Mac <rodrigocunhasilveira@MacBook-Air-de-Rodrigo.local>
2025-02-03 16:40:50 -0300

    erro era apenas uma letra

--- src/Abstratas/Pessoas.java
+++ src/Abstratas/Pessoas.java
@@ -6,6 +6,13 @@ public abstract class Pessoas {
 
     private String sobrenome;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
     public String getSobrenome() {
         return sobrenome;
@@ -13,13 +20,5 @@ public abstract class Pessoas {
 
     public void setSobrenome(String sobrenome) {
         this.sobrenome = sobrenome;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 }
--- src/Abstratas/Programa.java
+++ src/Abstratas/Programa.java
@@ -1,12 +1,13 @@
 package Abstratas;
 
+
 public class Programa {
 
     public static void main (String args[]){
 
         Pessoas pessoas = new PessoaF();
-        Pessoas.setNome("rodrigo");
-        Pessoas.setSobrenome("Silveira");
+        pessoas.setNome("rodrigo");
+        pessoas.setSobrenome("Silveira");
 
         System.out.println(pessoas.getNome() + " " + pessoas.getSobrenome());
     }
